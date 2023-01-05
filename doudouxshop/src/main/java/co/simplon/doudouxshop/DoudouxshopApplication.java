76ab6cf88074ed38e4b223dca541f0538b5dcc44
NicolasDrp <drapier.nicolas07@gmail.com>
2023-01-05 21:43:11 +0100

    premier commit

---
+++ doudouxshop/src/main/java/co/simplon/doudouxshop/DoudouxshopApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.doudouxshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoudouxshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoudouxshopApplication.class, args);
+	}
+
+}
---
+++ doudouxshop/src/test/java/co/simplon/doudouxshop/DoudouxshopApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.doudouxshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoudouxshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
